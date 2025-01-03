d379fb2e714acbcc78ec5da0ef3e5a5649074805
Henoke <henokemafm@gmail.com>
2025-01-03 03:31:48 +0300

    Main.ts is configured to create microservice, and App Controller is removed

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  
+
+  @MessagePattern({cmd:'new_incident'})
+  handleIncident(data:any){
+   console.log('New Incident received', data);
   }
 }
